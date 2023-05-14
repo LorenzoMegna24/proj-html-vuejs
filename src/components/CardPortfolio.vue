ce1a24abbd58479382cc64d6b8f74adf0731ec53
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-14 12:42:08 +0200

    Fix css card

--- src/components/CardPortfolio.vue
+++ src/components/CardPortfolio.vue
@@ -6,15 +6,20 @@
 </script>
 
 <template>
-  <div class="card rounded-4" style="width: 18rem;">
+  <div class="card rounded-4">
     <img class="rounded-4" :src="lavori.img" alt="">
-    <div class="card-body d-flex mb-0 pb-0">
+    <div class="card-body d-flex justify-content-between mb-0 pb-0">
       <h5 class="card-title">{{ lavori.title }}</h5>
-      <span class="card-text">{{ lavori.text }}</span>
+      <span>{{ lavori.text }}</span>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+  span{
+    font-size: 12px;
+  }
+  .card{
+    width: 350px;
+  }
 </style>
